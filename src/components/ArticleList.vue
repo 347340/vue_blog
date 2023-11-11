38b601c7dd60cea141542b672e41f318495a9aea
347340 <3473401075@qq.com>
2023-11-11 17:55:28 +0800

    增加了博客的列表页面

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p v-for="article in articles" :key="article.id">
+        {{ article.created_at }}&nbsp;&nbsp;
+        {{ article.title }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      articles: []
+    }
+  },
+  mounted () {
+    this.$http.get('http://localhost:3000/api/articles').then((response) => {
+      console.info(response.body)
+      this.articles = response.body.articles
+    }, (response) => {
+      console.error(response)
+    })
+  }
+}
+</script>
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+// import App from './App'
+import ArticleList from '@/components/ArticleList'
 import router from './router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+/*  el: '#app',
   router,
   components: { App },
   template: '<App/>'
+  */
+  el: '#articleList',
+  router,
+  components: { ArticleList },
+  template: '<ArticleList/>'
 })
